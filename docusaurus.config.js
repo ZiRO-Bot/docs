69d2516f4b60853f29544af3a43720a5723d2e21
ziro <palembani@gmail.com>
2023-02-21 15:43:59 +0700

    fix: Repo rename

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,12 +14,12 @@ const config = {
   url: 'https://bot.palembani.xyz',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docs',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'ZiRO-Bot', // Usually your GitHub org/user name.
-  projectName: 'Z3R0-Documentation', // Usually your repo name.
+  projectName: 'docs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
